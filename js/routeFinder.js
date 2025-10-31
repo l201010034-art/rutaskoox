a4fbd20a4ae0a0c9074261b6d3f2cf67d9799704
l201010034-art <l201010034@itlerma.edu.mx>
2025-10-30 22:18:15 -0600

    KooxR v1.1.29

--- js/routeFinder.js
+++ js/routeFinder.js
@@ -29,7 +29,7 @@ export function encontrarRutaCompleta(inicio, fin, todasLasRutas, mapRutaParader
     let solucionesEncontradas = [];
     let minTramosEncontrado = Infinity;
     let combinacionesEncontradas = new Set();
-    const limiteTransbordos = 8; 
+    const limiteTransbordos = 4; 
 
     const caminoBase = [{ tipo: 'caminar', paradero: inicio, texto: `Camina a: ${inicio.properties.nombre}` }];
     visitados.set(inicio.properties.nombre, 0);
@@ -109,7 +109,7 @@ export function encontrarRutaCompleta(inicio, fin, todasLasRutas, mapRutaParader
  * Enlaza Paraderos a Rutas usando Turf.js
  */
 export function linkParaderosARutas(paraderos, rutas) {
-    const DISTANCIA_MAXIMA = 20;
+    const DISTANCIA_MAXIMA = 40;
     paraderos.forEach(paradero => {
         paradero.properties.rutas = []; 
         rutas.forEach(ruta => {
--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // !! IMPORTANTE !!
 // ¡Asegúrate de cambiar esto a 'v1.5' (o lo que siga) para
 // que esta actualización se aplique!
-const CACHE_VERSION = 'v1.6'; // <-- ¡INCREMENTA ESTO!
+const CACHE_VERSION = 'v1.7'; // <-- ¡INCREMENTA ESTO!
 const CACHE_NAME = `rutas-koox-cache-${CACHE_VERSION}`;
 
 // Estos son los archivos MÍNIMOS para que la app "funcione" sin conexión.
